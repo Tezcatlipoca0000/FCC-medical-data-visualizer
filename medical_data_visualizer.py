285b98126592d8708a5b5c12740b3edab7834393
Tezcatlipoca0000 <tezcatlipoca0000@gmail.com>
2023-01-17 16:30:37 -0600

    Solved the challenge

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -7,8 +7,8 @@ import numpy as np
 df = pd.read_csv('medical_examination.csv')
 
 # Add 'overweight' column
-bmi = df['weight'] / (df['height'] / 100) ** 2
-df['overweight'] = bmi.apply(lambda x: 0 if x < 26 else 1)
+bmi = df['weight'] / ((df['height'] / 100) ** 2)
+df['overweight'] = bmi.apply(lambda x: 0 if x <= 25 else 1)
 
 # Normalize data by making 0 always good and 1 always bad. If the value of 'cholesterol' or 'gluc' is 1, make the value 0. If the value is more than 1, make the value 1.
 df['cholesterol'] = df['cholesterol'].apply(lambda x: 0 if x == 1 else 1)
@@ -25,12 +25,12 @@ def draw_cat_plot():
     
 
     # Draw the catplot with 'sns.catplot()'
-    sns.catplot(data=df_cat, kind='count', x='variable', hue='value', col='cardio')
+    g = sns.catplot(data=df_cat, kind='count', x='variable', hue='value', col='cardio')
+    g.set_axis_labels("variable", "total")
 
 
     # Get the figure for the output
-    cat_plot = sns.catplot(data=df_cat, kind='count', x='variable', hue='value', col='cardio')
-    fig = cat_plot.figure
+    fig = g.figure
 
 
     # Do not modify the next two lines
@@ -42,15 +42,10 @@ def draw_cat_plot():
 def draw_heat_map():
     # Clean the data
     global df
-    df = df[df['ap_lo'] <= df['ap_hi']]
-    df = df[df['height'] >= df['height'].quantile(0.025)]
-    df = df[df['height'] <= df['height'].quantile(0.975)]
-    df = df[df['weight'] >= df['weight'].quantile(0.025)]
-    df = df[df['weight'] <= df['weight'].quantile(0.975)]
-    df_heat = df.copy()
+    heat_df = df[(df['ap_lo'] <= df['ap_hi']) & (df['height'] >= df['height'].quantile(0.025)) & (df['height'] <= df['height'].quantile(0.975)) & (df['weight'] >= df['weight'].quantile(0.025)) & (df['weight'] <= df['weight'].quantile(0.975))]
 
     # Calculate the correlation matrix
-    corr = df_heat.corr()
+    corr = heat_df.corr()
 
     # Generate a mask for the upper triangle
     mask = np.triu(corr)
@@ -58,11 +53,11 @@ def draw_heat_map():
 
 
     # Set up the matplotlib figure
-    heat_map = sns.heatmap(corr, mask=mask, annot=True, fmt='.1f', center=0, vmax=0.3)
-    fig, ax = heat_map.figure()
+    #heat_map = sns.heatmap(corr, mask=mask, annot=True, fmt='.1f', center=0, vmax=0.3)
+    fig, ax = plt.subplots(figsize=(13,7))
 
     # Draw the heatmap with 'sns.heatmap()'
-    sns.heatmap(corr, mask=mask, annot=True, fmt='.1f', center=0, vmax=0.3)
+    sns.heatmap(corr, mask=mask, annot=True, fmt='.1f', center=0, vmax=0.3, ax=ax, linewidth=.5)
 
 
     # Do not modify the next two lines
